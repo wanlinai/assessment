4de0e277c2a505d80e7150e3a732f4cd69f20e6c
wanlinai <60218354+wanlinai@users.noreply.github.com>
2020-04-22 21:07:08 +0800

    #1に対する対応をしました

--- assessment.js
+++ assessment.js
@@ -82,7 +82,7 @@ function assessment(userName) {
   }
 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfcharCode % answers.length;
+  const index = sumOfcharCode % answers.length;　
   let result = answers[index];
 
   result = result.replace(/{userName}/g, userName);
